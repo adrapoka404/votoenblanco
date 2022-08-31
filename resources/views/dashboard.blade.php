669a5e787d64140e79c8ebd41b82d1556cc66ecf
adrapoka404 <adrapok@gmail.com>
2022-08-31 11:44:36 -0500

    Datos del rol en el dashaboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,4 +1,5 @@
 <x-app-layout>
+    {{Auth::user()->roles()->get()}}
     @if(empty(Auth::user()->roles()->get()))
     <div class=" bg-red-300 text-lg font-extralight">Sin Rol</div>    
     @endif
