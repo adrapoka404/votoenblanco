029faed2ebcfb95612ddd020598edf9356fa13a7
adrapoka404 <adrapok@gmail.com>
2022-09-13 19:53:44 -0500

    animacion subiendo imagen

--- resources/views/admin/posts/partials/form.blade.php
+++ resources/views/admin/posts/partials/form.blade.php
@@ -442,12 +442,12 @@
                 processData: false,
                 contentType: false,
                 success: function(data) {
-                    if (data.success) 
-                        select_image(data.to, data.img)
+                   // if (data.success) 
+                        //select_image(data.to, data.img)
                 },
                 beforeSend: function() {
                     console.log('Subiendo hacer algo');
-                    $("#popup_images").html('<div class="text-blue animate-spin ml-10" > Subiendo...</div>');
+                    $("#popup_images").html('<div class="text-blue animate-pulse ml-12 font-semibold" > Subiendo... </div>');
                 },
                 error: function(error) {
 
