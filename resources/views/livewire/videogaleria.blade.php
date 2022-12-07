7c03fc0333f74ba317651d1383d92040d119d681
adrapoka404 <adrapok@gmail.com>
2022-12-06 20:06:57 -0600

    videogaleria en carrusel medidass a 400

--- resources/views/livewire/videogaleria.blade.php
+++ resources/views/livewire/videogaleria.blade.php
@@ -6,13 +6,13 @@
             <div id="carouselVideoGaleria" class="carousel slide relative" data-bs-ride="carousel">
                 <div class="carousel-inner relative w-full overflow-hidden">
                     <div class="carousel-item active relative float-left w-full">
-                    <iframe height="386" width="100%" src="{{ $vgaleria->url }}" title="{{$vgaleria->title}}" frameborder="0"
+                    <iframe height="400" width="100%" src="{{ $vgaleria->url }}" title="{{$vgaleria->title}}" frameborder="0"
                         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                         allowfullscreen></iframe>
                     </div>
                     @foreach ($vgalerias as $indx => $vg)
                         <div class="carousel-item relative float-left w-full">
-                            <iframe height="386" width="100%" src="{{ $vg->url }}" title="{{$vg->title}}" frameborder="0"
+                            <iframe height="400" width="100%" src="{{ $vg->url }}" title="{{$vg->title}}" frameborder="0"
                                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                                 allowfullscreen></iframe>
                     </div>
