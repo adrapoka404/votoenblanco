ac104f539bf49ebf90e24979f792ddcb90a89179
adrapoka404 <adrapok@gmail.com>
2022-12-29 13:12:58 -0600

    detener referencias

--- app/Http/Controllers/Admin/StatisticsController.php
+++ app/Http/Controllers/Admin/StatisticsController.php
@@ -76,12 +76,13 @@ class StatisticsController extends Controller
         $references = [];
         
         $posts = Post::where('views', '>', 0)->orderBy('views', 'desc')->paginate(10);
-return $posts;
+
         foreach ($posts as $post) {
             $estadistocas = PostView::where('post_id', $post->id)->get();
             foreach ($estadistocas as $est)
                 $references[] = unserialize($est->reference);
         }
+        return $references;
         $mores_visit = [];
         $froms = [];
         $referers = [];
